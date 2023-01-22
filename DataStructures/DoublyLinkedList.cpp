dbc36f882d56a7518c13b28a98fb89da62ce273f
Hakan Acundaş <50059772+HakanAcundas@users.noreply.github.com>
2023-01-22 04:55:58 +0100

    Update DoublyLinkedList.cpp

--- DataStructures/DoublyLinkedList.cpp
+++ DataStructures/DoublyLinkedList.cpp
@@ -15,7 +15,7 @@ public:
 
 	void Insert(int value);
 	void Insert(int index, int value);
-	void Delete(int index, int value);
+	void Delete(int index);
 
 	void Print(bool isReverse);
 
@@ -56,6 +56,72 @@ void DoublyLinkedList::Insert(int value)
 	}
 }
 
+void DoublyLinkedList::Insert(int index, int value)
+{
+	Node* temp = new Node();
+	temp->value = value;
+	temp->next = nullptr;
+	temp->previous = nullptr;
+
+	if (head == nullptr)
+	{
+		head = temp;
+	}
+	else
+	{
+		Node* traverse = head;
+		for (int i = 0; i <= index - 2; i++)
+		{
+			if (traverse->next != nullptr)
+			{
+				traverse = traverse->next;
+			}
+		}
+		if (traverse->next == nullptr)
+		{
+			std::cout << "Index out of bound range for linked list!" << std::endl;
+		}
+		else 
+		{
+			temp->previous = traverse;
+			temp->next = traverse->next;
+			traverse->next->previous = temp;
+			traverse->next = temp;
+		}
+		
+	}
+}
+
+void DoublyLinkedList::Delete(int index)
+{
+	Node* traverse = head;
+	if (index == 0 && traverse->next != nullptr)
+	{
+		head = head->next;
+		return;
+	}
+
+	for (int i = 0; i <= index - 2; i++)
+	{
+		if (traverse->next->next == nullptr)
+		{
+			std::cout << "Please enter a valid number!" << std::endl;
+			return;
+		}
+		traverse = traverse->next;
+	}
+	if (traverse->next == nullptr)
+	{
+		std::cout << "Please enter a valid number!" << std::endl;
+		return;
+	}
+	else
+	{
+		traverse->next->previous = traverse;
+		traverse->next = traverse->next->next;
+	}
+}
+
 void DoublyLinkedList::Print(bool isReverse)
 {
 	
@@ -85,7 +151,13 @@ int main()
 	dbl.Insert(69);
 	dbl.Insert(2);
 	dbl.Insert(2);
-	dbl.Insert(31);
+	dbl.Insert(21);
 
-	dbl.Print(true);
+
+	dbl.Insert(2, 100);
+	dbl.Insert(2, 99);
+
+	dbl.Delete(3);
+
+	dbl.Print(false);
 }
